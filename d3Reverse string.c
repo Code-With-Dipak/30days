395d08757cd82f25d694dbde01868a5aefdd0bc9
Code With_Dipak <140307138+Code-With-Dipak@users.noreply.github.com>
2023-09-14 19:28:09 +0530

    Create d3Reverse string.c

---
+++ d3Reverse string.c
@@ -0,0 +1,17 @@
+// DAY3  //
+// string Reverse without using inbuilt  function //
+#include<stdio.h>
+main (){
+   char a[50] ;
+   clrscr();
+   printf (“enter a string:-”);
+   gets (a);
+   strrev (a);
+   printf(“reversed string:- = %s”,a)
+   getch ();
+}
+
+
+***************Output******************
+enter a string:- Hello
+reversed string:- = olleH
