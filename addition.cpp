184432b6faeb7aa3c6a706185c3141e51f53db9f
Code With_Dipak <140307138+Code-With-Dipak@users.noreply.github.com>
2023-09-14 19:13:25 +0530

    Create addition.cpp

---
+++ addition.cpp
@@ -0,0 +1,23 @@
+             // Day 1 //
+
+// addition of two number's whitout using 3rd number //
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    num1 = num1 + num2;
+
+    cout << "Sum: " << num1;
+
+    return 0;
+}
